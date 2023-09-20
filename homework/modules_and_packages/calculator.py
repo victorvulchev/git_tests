f3057d741fa50186e04c14a18c0c71b1d20555b5
Victor <victor.vulchev@gmail.com>
2023-09-20 14:52:00 +0300

    task 4

---
+++ homework/modules_and_packages/calculator.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    print(a + b)
+
+def subtract(a,b):
+    print(a - b)
+
+def multiplication(a, b):
+    print(a * b)
+
+def division(a,b):
+    print(a / b)
---
+++ homework/modules_and_packages/mods_packs_task4.py
@@ -0,0 +1,9 @@
+import calculator
+
+a = 5
+b = 5
+
+calculator.add(a, b)
+calculator.division(a, b)
+calculator.multiplication(a, b)
+calculator.subtract(a, b)
