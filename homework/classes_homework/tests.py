c4c1aa943741d3c8a3087e23417a13ddf8a81547
Victor <victor.vulchev@gmail.com>
2023-10-17 18:41:57 +0300

    test

--- homework/classes_homework/tests.py
+++ homework/classes_homework/tests.py
@@ -1,3 +1,17 @@
 import os
 
-print(f"OS: {os.name}")
+#print(f"OS: {os.name}")
+#print("___Here___")
+#print(os.getcwd())
+#print("___Here___")
+#print(os.listdir(path="."))
+#the_path = os.getcwd()
+#print(os.listdir(the_path))
+#print(os.path.isdir(the_path))
+f = open("text.txt", "w+")
+f.write("Does this work?")
+with open("text.txt", "r") as reader:
+    line = reader.readline()
+    while line != " ":
+        print(line, end=" ")
+        line = reader.readline()
