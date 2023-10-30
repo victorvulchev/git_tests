06182688f143847610af5ed680453a4499cf6312
Victor <victor.vulchev@gmail.com>
2023-10-30 21:42:52 +0200

    tests

---
+++ homework/graphs/new.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+temperatures = [22, 23, 24, 22, 20, 19, 21]
+
+plt.plot(days, temperatures)
+plt.show()
